206512f47ea58c3ba476d5f094b1a29f3cc1e1e2
OsipovStas <stasstels@gmail.com>
2024-10-29 14:19:36 +0300

    added using streamlit secrets

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -1,10 +1,14 @@
 import sys
 import os
 from dotenv import load_dotenv
+import streamlit as st
 # Add the project root directory to Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv(".env", override=True)
+# Iterate through all secrets and set them as environment variables
+for key, value in st.secrets.items():
+    os.environ[key] = str(value)
 
 # Import and run your actual Streamlit app
 from ui import chat
